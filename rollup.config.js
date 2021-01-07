0607039f1fac599f5abcf4e65f450892d7c3fc01
Tom Hinkle <tmhinkle@gmail.com>
2021-01-07 10:42:43 -0500

    ++URL test

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
-
+const NETLIFY_URL = process.env.URL || 'https://localhost:8888/'
 function serve() {
 	let server;
 
@@ -38,6 +39,9 @@ export default [
 			file: 'public/build/bundle.js'
 		},
 			plugins: [
+				replace({
+					NETLIFY_URL
+				}),
 				svelte({
 					compilerOptions: {
 						// enable run-time checks when not in production
@@ -57,7 +61,7 @@ export default [
 					browser: true,
 					dedupe: ['svelte']
 				}),
-				commonjs(),
+				commonjs(),				
 
 				// In dev mode, call `npm run start` once
 				// the bundle has been generated
--- src/Login.svelte
+++ src/Login.svelte
@@ -1,9 +1,16 @@
 <script>
     import Config from './Config.svelte';
+    let clientID = 34363;
+    let netlify_uri = 'NETLIFY_URL';
+    let uri = 'http://www.strava.com/oauth/authorize?client_id={clientID}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read'
     let authenticated = false;
+
+//$ http GET "https://www.strava.com/api/v3/athlete" "Authorization: Bearer [[token]]"
+
 </script>
+
 <div>
-    This will be a login component.
+    This will be a login component. {netlify_uri}
     
     {#if authenticated}
     <p>You are logged in</p>
