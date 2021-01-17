f2b58b18bf814258ae5e82816a17aaf829e22584
Tom Hinkle <tmhinkle@gmail.com>
2021-01-17 14:43:02 -0500

    Move $token to store

--- src/Config.svelte
+++ src/Config.svelte
@@ -1,4 +1,6 @@
 <script>
+    import {token} from './stores.js'
+
     let data;
     let message;
     const uri = '/.netlify/functions/api'
@@ -11,9 +13,22 @@
         );
         data = await response.json();
     }
+
+    function getAthleteData () {
+        
+    }
+    let athleteData
+
 </script>
 <div>
     <h2>Preferences</h2>
+    {#if $token}
+    We are ready to do API magic!
+    <button on:click={getAthleteData()}>Get athlete data!</button>
+    {athleteData}
+    {:else}
+    Not logged in yet :(
+    {/if}
     Message: <input type='text' bind:value={message}>
     <button on:click={getData}>Send Request</button>
     <div>{JSON.stringify(data)}</div>
--- src/Login.svelte
+++ src/Login.svelte
@@ -1,10 +1,31 @@
 <script>
     import Config from './Config.svelte';
+    import {token} from './stores.js';
     let clientID = 34363;
     let netlify_uri = 'NETLIFY_URL';
     let loginUri = `http://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${netlify_uri}/&approval_prompt=force&scope=read`
-    let authenticated = false;
+    let loginCode = new URLSearchParams(location.search).get('code')
+    let authenticated = $token;
+    if (loginCode) {
+        authenticated = 'Partial';
+        console.log('Part way authenticated...');
+        getToken(loginCode);
+    }
+    const uri = '/.netlify/functions/api'
 
+
+
+    async function getToken () {
+        console.log('Get token...');
+        let response = await fetch(uri,
+            {method : 'POST',
+            body : JSON.stringify(
+                {code:loginCode}
+            )}
+        );
+        let jsonResponse = await response.json();
+        $token = jsonResponse.token;
+    }
 //$ http GET "https://www.strava.com/api/v3/athlete" "Authorization: Bearer [[token]]"
 
 </script>
@@ -14,6 +35,7 @@
     
     {#if authenticated}
     <p>You are logged in</p>
+    <p>Wowza you got a token! {JSON.stringify($token)}</p>
     <button on:click={()=>authenticated=false}>Log out</button>
     <Config/>
     {:else}
