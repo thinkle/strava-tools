2440b6af5765fd435e4bdd8782d5bc79ab493638
Tom Hinkle <tmhinkle@gmail.com>
2021-04-08 16:25:09 -0400

    Fix empty date issue. ++prettier

--- src/DatePicker.svelte
+++ src/DatePicker.svelte
@@ -1,29 +1,32 @@
 <script type="typescript">
-  import { fade } from 'svelte/transition';
+  import { fade } from "svelte/transition";
   import { getActivities, getAthlete } from "./strava";
   import { startDate, endDate, activityFetcher } from "./activityStore";
   import { onMount } from "svelte";
   export let autoUpdate = true;
 
   //$endDate = new Date();
-  let end = $endDate && $endDate.toISOString().substr(0,10) || '';
+  let end = ($endDate && $endDate.toISOString().substr(0, 10)) || "";
   //$startDate = new Date($endDate.getFullYear(), $endDate.getMonth(), 1, 0);
-  let start = $startDate && $startDate.toISOString().substr(0,10) || '';
-  
-  $: if (autoUpdate) $startDate = useStartDate && new Date(start) || 0; // react to string changes
-  $: if (autoUpdate) $endDate = useEndDate && new Date(end) || 0; // react to string changes
+  let start = ($startDate && $startDate.toISOString().substr(0, 10)) || "";
 
-  let useStartDate = !!$startDate
-  let useEndDate = !!$endDate
+  $: if (autoUpdate && start)
+    $startDate = (useStartDate && new Date(start)) || 0; // react to string changes
+  $: if (autoUpdate && end) $endDate = (useEndDate && new Date(end)) || 0; // react to string changes
 
+  let useStartDate = !!$startDate;
+  let useEndDate = !!$endDate;
 </script>
+
 <div class="wrap">
-  From<input type="checkbox" bind:checked={useStartDate}> 
-    <input class:invisible={!useStartDate} type="date" bind:value={start} /> 
-  to<input type="checkbox" bind:checked={useEndDate}> 
-    <input class:invisible={!useEndDate} type="date" bind:value={end} /> 
+  From<input type="checkbox" bind:checked={useStartDate} />
+  <input class:invisible={!useStartDate} type="date" bind:value={start} />
+  to<input type="checkbox" bind:checked={useEndDate} />
+  <input class:invisible={!useEndDate} type="date" bind:value={end} />
   {#if useStartDate && useEndDate && start > end}
-    <div transition:fade class="warning">Oops! Your start date is after your end date!</div>
+    <div transition:fade class="warning">
+      Oops! Your start date is after your end date!
+    </div>
   {/if}
 </div>
 
@@ -33,7 +36,7 @@
     text-align: center;
     align-items: center;
   }
-  input[type=date] {
+  input[type="date"] {
   }
   input {
     opacity: 1;
--- src/TimeTally.svelte
+++ src/TimeTally.svelte
@@ -1,151 +1,172 @@
 <script type="typescript">
-  import DatePicker from './DatePicker.svelte';
-  import TimeRow from './TimeRow.svelte';
-  import ColorPicker from './ColorPicker.svelte';
-  import Activity from './Activity.svelte';
+  import DatePicker from "./DatePicker.svelte";
+  import TimeRow from "./TimeRow.svelte";
+  import ColorPicker from "./ColorPicker.svelte";
+  import Activity from "./Activity.svelte";
   import { getActivities, getAthlete } from "./strava";
-  import { startDate, endDate, activityFetcher } from './activityStore';
-  import {onMount} from 'svelte';
+  import { activityFetcher } from "./activityStore";
+  import { onMount } from "svelte";
   let colorUpdate = 0;
 
-  function getGearName (gear_id, athlete) {
+  function getGearName(gear_id, athlete) {
     if (athlete && athlete.bikes) {
-      let bike = athlete.bikes.find((b)=>b.id==gear_id);
+      let bike = athlete.bikes.find((b) => b.id == gear_id);
       if (bike) {
-        return bike.name
+        return bike.name;
       }
     }
-    return gear_id
+    return gear_id;
   }
 
-  let athlete
-  onMount(async ()=>{
-     athlete = await getAthlete()
+  let athlete;
+  onMount(async () => {
+    athlete = await getAthlete();
   });
 
-  let totalTime
+  let totalTime;
   let activities = [];
   //let allActivities = [];
   $: activities = $activityFetcher.activities();
-  let indoorTypes = new Set(['VirtualRide','VirtualRun','Yoga','Workout','WeightTraining']);
+  let indoorTypes = new Set([
+    "VirtualRide",
+    "VirtualRun",
+    "Yoga",
+    "Workout",
+    "WeightTraining",
+  ]);
   let activityTypes = [];
   let activityGears = [];
-  let byTypeMap = {}
-  let byGearMap = {}
+  let byTypeMap = {};
+  let byGearMap = {};
   let page = 1;
   let fetching = false;
-  function formatDuration (s) {
+  function formatDuration(s) {
     let hours = 0;
     let minutes = 0;
-    return s + ' seconds'
+    return s + " seconds";
   }
 
   $: {
     if (!fetching && !$activityFetcher.getFetcher().complete) {
-      async function update () {
+      async function update() {
         fetching = true;
-        await $activityFetcher.fetchMore()
+        await $activityFetcher.fetchMore();
         fetching = false;
       }
       update();
     }
   }
-  
+
   $: {
     byTypeMap = {};
     byGearMap = {};
     for (let a of activities) {
       if (byTypeMap[a.type]) {
-        byTypeMap[a.type].push(a)
+        byTypeMap[a.type].push(a);
       } else {
-        byTypeMap[a.type] = [a]
+        byTypeMap[a.type] = [a];
       }
       if (a.gear_id) {
         if (byGearMap[a.gear_id]) {
-          byGearMap[a.gear_id].push(a)
+          byGearMap[a.gear_id].push(a);
         } else {
-          byGearMap[a.gear_id] = [a]
+          byGearMap[a.gear_id] = [a];
         }
       }
     }
   }
-  function forceRerender () {
+  function forceRerender() {
     byTypeMap = byTypeMap;
   }
-  
+
   $: activityTypes = Object.keys(byTypeMap);
-  $: activityGears = Object.keys(byGearMap)
+  $: activityGears = Object.keys(byGearMap);
   let highlightedActivity;
-  let hx=0;
-  let hy=0;
-  function highlightActivity (a, event) {
-    if (!stickyHighlight || (stickyHighlight && event.type=='click')) {
+  let hx = 0;
+  let hy = 0;
+  function highlightActivity(a, event) {
+    if (!stickyHighlight || (stickyHighlight && event.type == "click")) {
       highlightedActivity = a;
-      if (event.type=='click' && a) {
+      if (event.type == "click" && a) {
         stickyHighlight = true;
-        console.log('sticky mode!')
+        console.log("sticky mode!");
       }
     } else {
-      console.log('ignoring event',event)
-      return
+      console.log("ignoring event", event);
+      return;
     }
     if (event) {
-      hx= Math.max(0,Math.min(event.clientX - (.5*window.innerWidth/2),window.innerWidth/2))
-      hy=Math.min(event.clientY+5,window.innerHeight-200)
+      hx = Math.max(
+        0,
+        Math.min(
+          event.clientX - (0.5 * window.innerWidth) / 2,
+          window.innerWidth / 2
+        )
+      );
+      hy = Math.min(event.clientY + 5, window.innerHeight - 200);
     }
   }
   let showByType;
   let showByGear;
   let stickyHighlight;
-
 </script>
+
 <div>
   <div class="top">
-  <h3>Total Outdoor Time</h3>
-  <div style="margin:auto"><DatePicker></DatePicker></div>
+    <h3>Total Outdoor Time</h3>
+    <div style="margin:auto"><DatePicker /></div>
   </div>
   <div class="fetchingNotification" class:active={fetching}>
     Fetching more activities from Strava...
   </div>
   {#if highlightedActivity}
-    <div class:stickyHighlight class="screen"
-    on:click={()=>{highlightedActivity=null;stickyHighlight=false}}
+    <div
+      class:stickyHighlight
+      class="screen"
+      on:click={() => {
+        highlightedActivity = null;
+        stickyHighlight = false;
+      }}
     >
-    <table class="highlight" style={`left:${hx}px;top:${hy}px`}
-      on:click={(e)=>{
-        if (stickyHighlight) {
-          e.stopPropagation();
-          console.log('caught click');
-        } else {
-          console.log('Clicked thingy');
-          stickyHighlight=true;
-          e.stopPropagation();
+      <table
+        class="highlight"
+        style={`left:${hx}px;top:${hy}px`}
+        on:click={(e) => {
+          if (stickyHighlight) {
+            e.stopPropagation();
+            console.log("caught click");
+          } else {
+            console.log("Clicked thingy");
+            stickyHighlight = true;
+            e.stopPropagation();
+          }
         }}
-      }
-    >
-      <Activity 
-      showCoordinateButton={false}
-      chooser={false} activity={highlightedActivity}/>
-    </table>
+      >
+        <Activity
+          showCoordinateButton={false}
+          chooser={false}
+          activity={highlightedActivity}
+        />
+      </table>
     </div>
   {/if}
   <table>
     <TimeRow
       activityType="Outdoor"
-      activities={activities.filter((a)=>!indoorTypes.has(a.type))}
+      activities={activities.filter((a) => !indoorTypes.has(a.type))}
       showCheck={false}
       {colorUpdate}
       {highlightActivity}
     />
     <TimeRow
       activityType="All Activities"
-      activities={activities}
+      {activities}
       {colorUpdate}
       showCheck={false}
       {highlightActivity}
     />
     <tr>
-      <th colspan="6" on:click={()=>showByType=!showByType}>
+      <th colspan="6" on:click={() => (showByType = !showByType)}>
         By type ({#if showByType}hide{:else}show{/if})
       </th>
     </tr>
@@ -154,46 +175,45 @@
         <TimeRow
           {colorUpdate}
           {activityType}
-          activities={activities.filter((a)=>a.type==activityType)}        
+          activities={activities.filter((a) => a.type == activityType)}
           {indoorTypes}
           onChange={forceRerender}
           {highlightActivity}
         >
-        <ColorPicker {athlete} colorId={activityType}
-          setColorCallback={()=>colorUpdate+=1}
-        ></ColorPicker>
-      </TimeRow>    
+          <ColorPicker
+            {athlete}
+            colorId={activityType}
+            setColorCallback={() => (colorUpdate += 1)}
+          />
+        </TimeRow>
       {/each}
     {/if}
     <tr>
-      <th colspan="6" on:click={()=>showByGear=!showByGear}>
+      <th colspan="6" on:click={() => (showByGear = !showByGear)}>
         By gear ({#if showByGear}hide{:else}show{/if})
       </th>
     </tr>
     {#if showByGear}
       {#each activityGears as activityGear}
         <TimeRow
-          activityType={getGearName(activityGear,athlete)}
-          activities={activities.filter((a)=>a.gear_id==activityGear)}        
+          activityType={getGearName(activityGear, athlete)}
+          activities={activities.filter((a) => a.gear_id == activityGear)}
           {indoorTypes}
           {colorUpdate}
-
           onChange={forceRerender}
           {highlightActivity}
         >
-        <ColorPicker 
-          {athlete} 
-          colorId={activityGear}
-          setColorCallback={()=>colorUpdate+=1}
-        ></ColorPicker>
-      </TimeRow>    
+          <ColorPicker
+            {athlete}
+            colorId={activityGear}
+            setColorCallback={() => (colorUpdate += 1)}
+          />
+        </TimeRow>
       {/each}
     {/if}
   </table>
 
-  <div class="spacer">
-    This space intentionally left blank.
-  </div>
+  <div class="spacer">This space intentionally left blank.</div>
 </div>
 
 <style>
@@ -202,7 +222,7 @@
     display: flex;
     align-items: flex-end;
     justify-content: center;
-    font-family:'Courier New', Courier, monospace;
+    font-family: "Courier New", Courier, monospace;
   }
   .top {
     background-color: white;
@@ -234,12 +254,12 @@
     transition: background-color 300ms;
   }
   .stickyHighlight {
-    position:fixed;
-    top:0;
-    left:0;
-    width:100vw;
-    height:100vh;
-    background-color:#fff9;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: #fff9;
   }
 
   /* Fetching notification */
@@ -260,5 +280,4 @@
   .fetchingNotification.active {
     opacity: 1;
   }
- 
 </style>
