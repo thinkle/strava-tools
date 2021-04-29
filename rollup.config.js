a4c92d6afedaae90551a93969324887dcc4fabeb
Tom Hinkle <tmhinkle@gmail.com>
2021-04-28 21:51:52 -0400

    Stop minifying production build so we can see what's going on w/ the bundle...

--- rollup.config.js
+++ rollup.config.js
@@ -1,101 +1,105 @@
-import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import css from 'rollup-plugin-css-only';
+import svelte from "rollup-plugin-svelte";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "rollup-plugin-replace";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import css from "rollup-plugin-css-only";
 // Typescript support
-import autoPreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
+import autoPreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
 
 const production = !process.env.ROLLUP_WATCH;
-const NETLIFY_URL = process.env.URL || 'http://localhost:8888'
-const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN
-const STRAVA_CLIENT_ID = process.env.strava_client_id
+const NETLIFY_URL = process.env.URL || "http://localhost:8888";
+const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
+const STRAVA_CLIENT_ID = process.env.strava_client_id;
 
 function serve() {
-	let server;
+  let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+  function toExit() {
+    if (server) server.kill(0);
+  }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require("child_process").spawn(
+        "npm",
+        ["run", "start", "--", "--dev"],
+        {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        }
+      );
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
+    },
+  };
 }
 
 export default [
-	{
-		input: 'src/main.js',
-		output: {
-			sourcemap: true,
-			format: 'iife',
-			name: 'app',
-			file: 'public/build/bundle.js'
-		},
-			plugins: [
-				replace({
-					NETLIFY_URL,
-					MAPBOX_TOKEN,
-					STRAVA_CLIENT_ID
-				}),
-				svelte({
-					compilerOptions: {
-						// enable run-time checks when not in production
-						dev: !production
-					},
-					preprocess: autoPreprocess()
-				}),
-				typescript({ sourceMap: !production }),
-				// we'll extract any component CSS out into
-				// a separate file - better for performance
-				css({ output: 'bundle.css' }),
+  {
+    input: "src/main.js",
+    output: {
+      sourcemap: true,
+      format: "iife",
+      name: "app",
+      file: "public/build/bundle.js",
+    },
+    plugins: [
+      replace({
+        NETLIFY_URL,
+        MAPBOX_TOKEN,
+        STRAVA_CLIENT_ID,
+      }),
+      svelte({
+        compilerOptions: {
+          // enable run-time checks when not in production
+          dev: !production,
+        },
+        preprocess: autoPreprocess(),
+      }),
+      typescript({ sourceMap: !production }),
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css({ output: "bundle.css" }),
 
-				// If you have external dependencies installed from
-				// npm, you'll most likely need these plugins. In
-				// some cases you'll need additional configuration -
-				// consult the documentation for details:
-				// https://github.com/rollup/plugins/tree/master/packages/commonjs
-				resolve({
-					browser: true,
-					dedupe: ['svelte']
-				}),
-				commonjs(),				
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        browser: true,
+        dedupe: ["svelte"],
+      }),
+      commonjs(),
 
-				// In dev mode, call `npm run start` once
-				// the bundle has been generated
-				!production && serve(),
+      // In dev mode, call `npm run start` once
+      // the bundle has been generated
+      !production && serve(),
 
-				// Watch the `public` directory and refresh the
-				// browser on changes when not in production
-				!production && livereload('public'),
+      // Watch the `public` directory and refresh the
+      // browser on changes when not in production
+      !production && livereload("public"),
 
-				// If we're building for production (npm run build
-				// instead of npm run dev), minify
-				production && terser()
-			],
-			watch: {
-				clearScreen: false
-			}
-		},
-	{
-		input : 'src/functions/api.js',
-		output : {
-			sourcemap : true,
-			format: 'cjs',
-			name : 'functions',
-			dir : 'functions'
-		}
-	}
-]
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      // production && terser()
+    ],
+    watch: {
+      clearScreen: false,
+    },
+  },
+  {
+    input: "src/functions/api.js",
+    output: {
+      sourcemap: true,
+      format: "cjs",
+      name: "functions",
+      dir: "functions",
+    },
+  },
+];
